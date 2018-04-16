7ea09a4abfee103dc088d8b6f533b82fe87c4c37
Maxime Duchene <duchene.maxime@outlook.fr>
2018-04-16 15:22:36 +0200

    TP03 fini

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,6 @@
 package dev;
 
+import java.util.ResourceBundle;
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -11,7 +12,23 @@ public class App
     public static void main( String[] args )
     {
         //System.out.println( "Hello World!" );
+    	/* TP 01
         String asciiArt1 = FigletFont.convertOneLine("Hello World");
         System.out.println(asciiArt1);
+         */
+    	/* TP 02
+    	String titre = ResourceBundle.getBundle("application").getString("titre");
+    	String asciiArt = FigletFont.convertOneLine(titre);
+    	System.out.println(asciiArt);
+    	*/
+    	
+    	//TP 03
+    	String titre = ResourceBundle.getBundle("application").getString("titre");
+    	String asciiArt = FigletFont.convertOneLine(titre);
+    	System.out.println(asciiArt);
+    	// affichage de l'environnement
+    	String environnement = ResourceBundle.getBundle("application").getString(
+    	"environnement");
+    	System.out.println("Environnement : " + environnement);
     }
 }
